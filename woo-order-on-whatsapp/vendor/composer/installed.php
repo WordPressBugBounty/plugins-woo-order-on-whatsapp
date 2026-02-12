a5f3e91afcc76751d072b597577ce58c936e0701
Sean Murphy <sean@iamseanmurphy.com>
2026-02-12 21:21:01 +0000

    2.9

---
+++ woo-order-on-whatsapp/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'eduardovillao/woo-order-on-whatsapp',
+        'pretty_version' => 'v2.9',
+        'version' => '2.9.0.0',
+        'reference' => 'acf6266a17500f08995c1cbdb4c0dfe8b6a66258',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'eduardovillao/woo-order-on-whatsapp' => array(
+            'pretty_version' => 'v2.9',
+            'version' => '2.9.0.0',
+            'reference' => 'acf6266a17500f08995c1cbdb4c0dfe8b6a66258',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'eduardovillao/wpfeatureloop-sdk' => array(
+            'pretty_version' => 'v1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => '923fbe0d40ba0a8756feb374932271e470518cc6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eduardovillao/wpfeatureloop-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
